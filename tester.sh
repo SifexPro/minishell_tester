0b20ee60e62ed50c59b255b04f0d3358faf56f83
SifexPro <sifexdesign@gmail.com>
2025-01-21 22:50:14 +0100

    Files check & some

--- tester.sh
+++ tester.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+###################### CONFIG ######################
 LOG=1
-CLEAN=1
+CLEAN=0
 tests_dir="tests"
 
-# ajouter tests d'erreur
+###################### TESTS ######################
+
+########### COMMANDS ###########
 commands=(
     "cat $tests_dir/test1"
 	"< $tests_dir/test1 wc -l"
@@ -24,7 +27,9 @@ commands=(
 	#"echo \$MY_VAR"
 	"<< stop wc -w\n1 2 3\n4 5\n6\nstop"
 )
+# ajouter tests d'erreur
 
+########### EXPECTED RESULTS ###########
 expected_results=(
     "a\na\na\na\na" #0
 	"5" #1
@@ -45,6 +50,11 @@ expected_results=(
 	"6"
 )
 
+########### FILES CHECKS ###########
+file_contents_check=("hello"
+"5\nappend\n5")
+
+########### TESTER ###########
 num_commands=${#commands[@]}
 
 file_contents=("a\na\na\na\na"
@@ -62,12 +72,17 @@ for ((i = 0; i < ${#file_contents[@]}; i++)); do
     file_name="test$((i + 1))"
     file_path="$tests_dir/$file_name"
 	echo -e "${file_contents[i]}" > "$file_path"
-    
-    # Confirmation
-    echo "Fichier '$file_path' créé avec succès."
+done
+
+for ((i = 0; i < ${#file_contents_check[@]}; i++)); do
+    file_name="test_check$((i + 1))"
+    file_path="$tests_dir/file_res/$file_name"
+	echo -e "${file_contents_check[i]}" > "$file_path"
 done
 
 failures=0
+
+echo -e "\\nCommands:"
 
 for ((i=0; i<num_commands; i++)); do
 	clean_command=$(echo "${commands[i]}" | sed 's/\\n/\n/g')
@@ -98,10 +113,27 @@ for ((i=0; i<num_commands; i++)); do
     echo "--------------------------------------"
 done
 
+echo -e "\\nFILES:"
+
+for ((i = 0; i < ${#file_contents_check[@]}; i++)); do
+    file1_name="test$((i + 1))"
+    file2_name="test_check$((i + 1))"
+
+    file1="$tests_dir/file_res/$file1_name"
+	file2="$tests_dir/file_res/$file2_name"
+
+	if cmp -s $file1 $file2; then
+		echo "✅ : $file1_name == $file2_name"
+	else
+		echo "❌ : $file1_name != $file2_name"
+		failures=$((failures + 1))
+	fi
+done
+
 echo -e "\\n\\nRESULTS:"
-echo "✅ : $((num_commands - failures))/$num_commands"
+echo "✅ : $((num_commands + ${#file_contents_check[@]} - failures))/$((num_commands + ${#file_contents_check[@]}))"
 if [ "$failures" -gt 0 ]; then
-	echo "❌ : $failures/$num_commands"
+	echo "❌ : $failures/$((num_commands + ${#file_contents_check[@]}))"
 fi
 
 # Clean up
